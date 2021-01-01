8d95398bddc158a910ca8816f673b979e032c833
Parker Jones <parker.harold.jones@gmail.com>
2020-12-31 22:14:13 -0700

    Added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { blogs: [] }
+
+  componentDidMount() {
+
+  }
+
+  addBlog = (incomingBlog) => {
+
+  }
+
+  updateBlog = (id, incomingBlog) => {
+
+  }
+
+  removeBlog = (id) => {
+
+  }
+
+  render () {
+    return (
+      <Container> 
+
+      </Container> 
+    )
+  }
 }
 
 export default App;
